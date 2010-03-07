d2a5f199f76b7e4fc8a99ed595dbaa511c1c6fce
Julian_Chu <walkingice@0xlab.org>
2010-03-08 02:37:51 +0800

    add CaseGLCube

---
+++ src/org/zeroxlab/benchmark/CaseGLCube.java
@@ -0,0 +1,47 @@
+package org.zeroxlab.benchmark;
+
+import android.util.Log;
+
+import org.zeroxlab.kubench.Kubench;
+
+import android.os.SystemClock;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import java.nio.*;
+
+public class CaseGLCube extends Case{
+
+    CaseGLCube() {
+	super("CaseGLCube", Kubench.getFullClassName(), 3, 80);
+    }
+
+    public String getDescription() {
+	return "OpenGL Cube benchmark";
+    }
+
+    @Override
+    public String getBenchmark() {
+	if (!isFinish()) {
+	    return TAG + " not finish yet";
+	}
+
+	String result = "";
+	long total = 0;
+	int length = mResult.length;
+
+	for (int i = 0; i < length; i++) {
+	    long fps = mCaseRound / (mResult[i] / 1000); // milliseconds to seconds
+	    result += "Round " + i + ": fps = " + fps + "\n";
+	    total  += fps;
+	}
+
+	result += "Average: fps = " + (total/length) + "\n";
+	return result;
+    }
+}
