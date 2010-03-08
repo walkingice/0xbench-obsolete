367087d4495fc0f52b572745887f121293ae4cb5
Julian_Chu <walkingice@0xlab.org>
2010-03-08 16:49:21 +0800

    GC extends Tester
    It override method finishTester because it only report String
    to class Case.

--- src/org/zeroxlab/benchmark/GC.java
+++ src/org/zeroxlab/benchmark/GC.java
@@ -1,4 +1,4 @@
-package org.zeroxlab.ZeroXBench;
+package org.zeroxlab.benchmark;
 
 import org.zeroxlab.gc.GCBenchmark;
 
@@ -7,14 +7,49 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.content.Intent;
 import android.widget.TextView;
 
-public class GC extends Activity {
+public class GC extends Tester{
 
 	private TextView mTextView1;
 
 	public static Handler mHandler;
 	public static final int GUINOTIFIER = 0x1234;
+
+
+	protected String getTag() {
+	    return "GC";
+	}
+
+	protected int sleepBeforeStart() {
+	    return 1000;
+	}
+
+	protected int sleepBetweenRound() {
+	    return 200;
+	}
+
+	protected void oneRound() {
+	    GCBenchmark.benchmark();
+	    decreaseCounter();
+	}
+
+	@Override
+	public void finishTester(long start, long end) {
+	    Intent intent = new Intent();
+	    if (mSourceTag == null || mSourceTag.equals("")) {
+		Case.putSource(intent, "unknown");
+	    } else {
+		Case.putSource(intent, mSourceTag);
+	    }
+
+	    Case.putIndex(intent, mIndex);
+
+	    intent.putExtra(CaseGC.GCRESULT, GCBenchmark.out.toString());
+	    setResult(0, intent);
+	    finish();
+	}
 
 	/** Called when the activity is first created. */
 	@Override
@@ -39,10 +74,6 @@ public class GC extends Activity {
 	@Override
 	public void onResume() {
 		super.onResume();
-		new Thread() {
-			public void run() {
-				GCBenchmark.benchmark();
-			}
-		}.start();
+		startTester();
 	}
 }
