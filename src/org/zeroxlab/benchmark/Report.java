af2deed6967adb30c18e59c3f41bc5516d554ffe
Julian_Chu <walkingice@0xlab.org>
2010-03-08 18:05:12 +0800

    add Report.java
    it is an activity which be used to show benchmarking report

---
+++ src/org/zeroxlab/benchmark/Report.java
@@ -0,0 +1,44 @@
+package org.zeroxlab.benchmark;
+
+import android.util.Log;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.content.*;
+import java.nio.*;
+
+import java.util.LinkedList;
+
+/* Construct a basic UI */
+public class Report extends Activity {
+
+    public final static String TAG = "Repord";
+    public final static String REPORT = "REPORT";
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+	super.onCreate(bundle);
+	setContentView(R.layout.report);
+
+	mTextView = (TextView)findViewById(R.id.report_text);
+
+	Intent intent = getIntent();
+	String report = intent.getStringExtra(REPORT);
+
+	if (report == null || report.equals("")) {
+	    mTextView.setText("oooops...report not found");
+	} else {
+	    mTextView.setText(report);
+	}
+    }
+
+    public static String fullClassName() {
+	return "org.zeroxlab.benchmark.Report";
+    }
+
+    public static String packageName() {
+	return "org.zeroxlab.benchmark";
+    }
+}
