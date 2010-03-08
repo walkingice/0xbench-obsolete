313f73daa1a7c3820af9d2967103e1fcb7d082c9
Julian_Chu <walkingice@0xlab.org>
2010-03-08 14:44:35 +0800

    add CaseDrawCircle

---
+++ src/org/zeroxlab/benchmark/CaseDrawCircle.java
@@ -0,0 +1,45 @@
+package org.zeroxlab.benchmark;
+
+import android.util.Log;
+
+import android.os.SystemClock;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import java.nio.*;
+
+public class CaseDrawCircle extends Case{
+
+    CaseDrawCircle() {
+	super("CaseDrawCircle", "org.zeroxlab.graphics.DrawCircle", 1, 300);
+    }
+
+    public String getDescription() {
+	return "Hi";
+    }
+
+    @Override
+    public String getBenchmark() {
+	if (!couldFetchReport()) {
+	    return "DrawCircle has no report";
+	}
+
+	String result = "";
+	long total = 0;
+	int length = mResult.length;
+
+	for (int i = 0; i < length; i++) {
+	    long fps = mCaseRound / (mResult[i] / 1000); // milliseconds to seconds
+	    result += "Round " + i + ": fps = " + fps + "\n";
+	    total  += fps;
+	}
+
+	result += "Average: fps = " + (total/length) + "\n";
+	return result;
+    }
+}
