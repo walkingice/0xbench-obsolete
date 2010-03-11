994fb48a389c5ef23b40dcc929a5cfcd8c571b4a
Julian_Chu <walkingice@0xlab.org>
2010-03-11 23:47:36 +0800

    Cube: correct the resource path

--- src/com/nea/nehe/lesson08/Cube.java
+++ src/com/nea/nehe/lesson08/Cube.java
@@ -225,7 +225,7 @@ public class Cube {
 	 */
 	public void loadGLTexture(GL10 gl, Context context) {
 		//Get the texture from the Android resource directory
-		InputStream is = context.getResources().openRawResource(R.drawable.glass);
+		InputStream is = context.getResources().openRawResource(org.zeroxlab.benchmark.R.drawable.glass);
 		Bitmap bitmap = null;
 		try {
 			//BitmapFactory is an Android graphics utility for images
