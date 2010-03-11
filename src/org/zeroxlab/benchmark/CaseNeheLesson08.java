78a0bd8dc3f90a249f5cb30c91589ef8a06b30b4
Julian_Chu <walkingice@0xlab.org>
2010-03-12 00:39:24 +0800

    Nehe 08: Add CaseNeheLesson08

---
+++ src/org/zeroxlab/benchmark/CaseNeheLesson08.java
@@ -0,0 +1,50 @@
+package org.zeroxlab.benchmark;
+
+import android.util.Log;
+
+import com.nea.nehe.lesson08.Run;
+import android.os.SystemClock;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class CaseNeheLesson08 extends Case {
+
+    public static int mNeheRepeat = 2;
+    public static int mNeheRound  = 1000;
+
+    CaseNeheLesson08() {
+	super("NeheLesson08", Run.FullName, mNeheRepeat, mNeheRound);
+    }
+
+    public String getTitle() {
+	return "OpenGL Blending";
+    }
+
+    public String getDescription() {
+	return "A very famous OpenGL tutorial to demo OpenGL blending";
+    }
+
+    @Override
+    public String getBenchmark() {
+	if (!couldFetchReport()) {
+	    return "Nehe Lesson 8 has no report";
+	}
+
+	String result = "";
+	float total = 0;
+	int length = mResult.length;
+
+	for (int i = 0; i < length; i++) {
+	    float fps = mCaseRound / (mResult[i] / 1000); // milliseconds to seconds
+	    result += "Round " + i + ": fps = " + fps + "\n";
+	    total  += fps;
+	}
+
+	result += "Average: fps = " + (total/length) + "\n";
+	return result;
+    }
+}
