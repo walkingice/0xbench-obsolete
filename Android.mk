d74dd8102c1f442820d39872d4762514eafcb6a6
Jim Huang <jserv@0xlab.org>
2010-03-29 18:22:02 +0800

    [bench] Rename package name from ZeroX_Benchmark to ZeroXBench
    Change-Id: Id609d68f542931bdb5f568ab47df993efbb96568

--- Android.mk
+++ Android.mk
@@ -21,7 +21,7 @@ LOCAL_MODULE_TAGS := user
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-LOCAL_PACKAGE_NAME := ZeroX_Benchmark
+LOCAL_PACKAGE_NAME := ZeroXBenchmark
 LOCAL_CERTIFICATE := shared
 
 include $(BUILD_PACKAGE)
