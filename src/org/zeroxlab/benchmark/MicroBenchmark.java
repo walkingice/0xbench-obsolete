e4fedd9ce12d3d044e9d01dbcddd75bc67442b72
Joseph Chang (bizkit) <bizkit@0xlab.org>
2010-05-28 18:24:28 +0800

    [bench] add MicroBenchmark class

---
+++ src/org/zeroxlab/benchmark/MicroBenchmark.java
@@ -0,0 +1,46 @@
+package org.zeroxlab.benchmark;
+
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.util.Log;
+
+/* code adapted from Caliper Project */
+
+class MicroBenchmark {
+	public static String upload(String xml, String postUrl, String apiKey, String benchmarkName) {
+    try {
+		URL url = new URL(postUrl + apiKey + "/" + benchmarkName);
+		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+		urlConnection.setDoOutput(true);
+
+		OutputStream post = urlConnection.getOutputStream();
+		post.write(xml.getBytes());
+
+		if (urlConnection.getResponseCode() == 200) {
+			Log.e("bzlog", "View current and previous benchmark results online:");
+			BufferedReader in = new BufferedReader(
+				new InputStreamReader(urlConnection.getInputStream()));
+			Log.e("bzlog", "  " + in.readLine());
+			return "";
+		}
+
+		Log.e("bzlog", "Posting to " + postUrl + " failed: "
+		  + urlConnection.getResponseMessage());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			Log.e("bzlog", line);
+		}
+
+		return "";
+    } catch (IOException e) {
+		throw new RuntimeException(e);
+    }
+	}
+}
+
