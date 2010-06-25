e826e14b02d7232939309510f82ff8057acc4b6a
Joseph Chang (bizkit) <bizkit@0xlab.org>
2010-06-25 17:44:38 +0800

    [bench] use thread to monitor stdout/err, display on screen

--- src/org/zeroxlab/benchmark/NativeTester.java
+++ src/org/zeroxlab/benchmark/NativeTester.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import android.app.Activity;
 import android.util.Log;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.os.Handler;
+import android.os.Message;
 import android.content.Intent;
 import android.widget.TextView;
 
@@ -16,14 +19,20 @@ public abstract class NativeTester extends Tester {
     private TextView mTextView;
     
     private Runtime mRuntime;
-    private BufferedReader stdout;
-    private BufferedReader stderr;
     private Process P;
 
     public String TAG = "NativeTester";
     public int UNFINISHED = -999;
 
     public String mCommand;
+    public Handler mHandler;
+    public static final int GUINOTIFIER = 0x1234;
+
+    private BufferedReader stdOutReader;
+    private BufferedReader stdErrReader;
+
+    private StringBuffer stdOut = new StringBuffer("stdout:\n");
+    private StringBuffer stdErr = new StringBuffer("stderr:\n");
 
     NativeTester() {
         mRuntime = Runtime.getRuntime();
@@ -34,6 +43,16 @@ public abstract class NativeTester extends Tester {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gc);
         mTextView = (TextView) findViewById(R.id.myTextView1);
+
+        mHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case GUINOTIFIER:
+                        mTextView.setText(stdErr.toString() + stdOut.toString());
+                        break;
+                }
+            }
+        };
     }
 
     protected abstract String getCommand();
@@ -51,22 +70,27 @@ public abstract class NativeTester extends Tester {
             P = mRuntime.exec(getCommand());
         } catch (Exception e) {
             Log.e(TAG, "Cannot execute");
+            mNow = 0;
+            finish();
         }
-        stdout = new BufferedReader(new InputStreamReader(P.getInputStream()));
-        stderr = new BufferedReader(new InputStreamReader(P.getErrorStream()));
+        stdOutReader = new BufferedReader(new InputStreamReader(P.getInputStream()));
+        stdErrReader = new BufferedReader(new InputStreamReader(P.getErrorStream()));
 
+        updateBuffer stdOutThread = new updateBuffer(stdOutReader, stdOut);
+        updateBuffer stdErrThread = new updateBuffer(stdErrReader, stdErr);
+        stdOutThread.start();
+        stdErrThread.start();
+
+        // wait here
         try {
             P.waitFor();
         } catch (InterruptedException e) {
             Log.d(TAG, "caller inturrupted");
             interruptTester();
         }
-        try {
-            Log.i(TAG, "stdout: " + getStdOut() );
-            Log.i(TAG, "stderr: " + getErrOut() );
-        } catch (IOException e) {
-            Log.e(TAG, "pipe error: " + e.toString());
-        }
+        Log.i(TAG, "stdout: " + stdOut );
+        Log.i(TAG, "stderr: " + stdErr );
+        SystemClock.sleep(5000); //remove me
         decreaseCounter();
     }
 
@@ -90,29 +114,35 @@ public abstract class NativeTester extends Tester {
     public void kill() {
         P.destroy();
     }
-    
-    public String getStdOut () throws IOException {
-        String buff = "";
-        String line = "";
-        while ( (line = stdout.readLine()) != null ) {
-            buff = buff + "\n" + line;
-        }
-        return buff;
-    }
-    
-    public String getErrOut () throws IOException {
-        String buff = "";
-        String line = "";
-        while ( (line = stdout.readLine()) != null ) {
-            buff = buff + "\n" + line;
-        }
-        return buff;
-    }
 
-    class TimedReader extends Thread {
+    class updateBuffer extends Thread {
         long mLastRead;
-        String mBuffer = "";
-        TimedReader(InputStreamReader is) {
+        BufferedReader is;
+        StringBuffer mBuffer;
+        final int UNREAD = -1;
+
+        updateBuffer(BufferedReader is, StringBuffer targetBuffer) {
+            this.is = is;
+            mBuffer = targetBuffer;
+            mLastRead = UNREAD;
+        }
+
+        public void run() {
+            String line;
+            try {
+                while ( (line = stdOutReader.readLine()) != null ) {
+                    mLastRead = SystemClock.uptimeMillis();
+                    mBuffer.append(line + '\n');
+                    Message m = new Message();
+                    m.what = GUINOTIFIER;
+                    mHandler.sendMessage(m);
+                }
+            } catch (IOException e) {
+            }
+        }
+
+        public long idleTime() {
+            return SystemClock.uptimeMillis() - mLastRead;
         }
     }
 }
