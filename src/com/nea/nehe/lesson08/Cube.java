63d8f858443026df9421d98e2cb6c3eb0197864c
Jim Huang <jserv@meow.(none)>
2010-08-02 11:31:09 +0800

    Added copyright notice to external source taken from Jeff Molofee (NeHe)

--- src/com/nea/nehe/lesson08/Cube.java
+++ src/com/nea/nehe/lesson08/Cube.java
@@ -1,3 +1,16 @@
+/*
+ * Authors Name: Jeff Molofee (NeHe)
+ *
+ * Disclaimer:
+ * This program may crash your system or run poorly depending on your
+ * hardware.  The program and code contained in this archive was scanned
+ * for virii and has passed all test before it was put online.  If you
+ * use this code in project of your own, send a shout out to the author!
+ *
+ * Ported to Android by INsanityDesign:
+ *  http://insanitydesign.com/wp/projects/nehe-android-ports/
+ */
+
 package com.nea.nehe.lesson08;
 
 import java.io.IOException;
